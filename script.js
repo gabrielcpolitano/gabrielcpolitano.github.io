3401fc439484384b78e715f44b2c38094a16dd69
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-06-05 13:27:42 -0300

    Obrigado Deus

--- script.js
+++ script.js
@@ -78,6 +78,16 @@ if(target.length) {
     },10))
 }
 
+// Example of smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
 
 
